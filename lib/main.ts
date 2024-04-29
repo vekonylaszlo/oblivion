f2aff1b7143514559fe2e269d776cd2d39411f9f
Vékony László <vekony.laszlo@biotechusa.com>
2024-04-29 17:20:59 +0200

    Added AlertDialog, Dropdown, Command, Toggle, Table components

--- lib/main.ts
+++ lib/main.ts
@@ -3,6 +3,19 @@ import { Avatar, AvatarImage, AvatarFallback } from "./components/avatar"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/select'
 import { Progress } from './components/progress'
 import { Tooltip, TooltipContent, TooltipTrigger } from './components/tooltip'
+import {
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from './components/alert-dialog'
 import { Textarea } from './components/textarea'
 import { Label } from './components/label'
 import { Calendar } from './components/calendar'
@@ -14,6 +27,26 @@ import { Button } from './components/button'
 import { Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField } from './components/form'
 import { Dialog, DialogClose, DialogContent, DialogTitle, DialogFooter, DialogHeader, DialogPortal, DialogTrigger, DialogOverlay, DialogDescription } from './components/dialog'
 import { Popover, PopoverTrigger, PopoverContent } from './components/popover'
+import { Table, TableRow, TableBody, TableCell, TableHead, TableFooter, TableHeader, TableCaption } from './components/table'
+import { Toggle } from './components/toggle'
+import { Command, CommandItem, CommandList, CommandEmpty, CommandGroup, CommandInput, CommandDialog, CommandShortcut, CommandSeparator } from "./components/command"
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+} from './components/dropdown-menu'
 
 
 export {
@@ -34,4 +67,35 @@ export {
   Calendar,
   Dialog, DialogClose, DialogContent, DialogTitle, DialogFooter, DialogHeader, DialogPortal, DialogTrigger, DialogOverlay, DialogDescription,
   Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField,
+  Table, TableRow, TableBody, TableCell, TableHead, TableFooter, TableHeader, TableCaption,
+  Toggle,
+  Command, CommandItem, CommandList, CommandEmpty, CommandGroup, CommandInput, CommandDialog, CommandShortcut, CommandSeparator,
+
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+
+  AlertDialog,
+  AlertDialogPortal,
+  AlertDialogOverlay,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
 }

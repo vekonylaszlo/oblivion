016bea4edf7969a3fbefd32a67a94138fdec8ddf
Vékony László <vekony.laszlo@biotechusa.com>
2024-04-29 21:44:24 +0200

    housekeeping

--- lib/main.ts
+++ lib/main.ts
@@ -3,6 +3,8 @@ import { Avatar, AvatarImage, AvatarFallback } from "./components/avatar"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/select'
 import { Progress } from './components/progress'
 import { Tooltip, TooltipContent, TooltipTrigger } from './components/tooltip'
+
+import { Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField } from './components/form'
 import {
   AlertDialog,
   AlertDialogPortal,
@@ -15,21 +17,7 @@ import {
   AlertDialogDescription,
   AlertDialogAction,
   AlertDialogCancel,
-} from './components/alert-dialog'
-import { Textarea } from './components/textarea'
-import { Label } from './components/label'
-import { Calendar } from './components/calendar'
-import { Switch } from './components/switch'
-import { Alert, AlertDescription, AlertTitle } from './components/alert'
-import { Checkbox } from './components/checkbox'
-import { Input } from './components/input'
-import { Button } from './components/button'
-import { Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField } from './components/form'
-import { Dialog, DialogClose, DialogContent, DialogTitle, DialogFooter, DialogHeader, DialogPortal, DialogTrigger, DialogOverlay, DialogDescription } from './components/dialog'
-import { Popover, PopoverTrigger, PopoverContent } from './components/popover'
-import { Table, TableRow, TableBody, TableCell, TableHead, TableFooter, TableHeader, TableCaption } from './components/table'
-import { Toggle } from './components/toggle'
-import { Command, CommandItem, CommandList, CommandEmpty, CommandGroup, CommandInput, CommandDialog, CommandShortcut, CommandSeparator } from "./components/command"
+} from "./components/alert-dialog"
 import {
   DropdownMenu,
   DropdownMenuTrigger,
@@ -48,55 +36,21 @@ import {
   DropdownMenuRadioGroup,
 } from './components/dropdown-menu'
 import { Badge } from "./components/badge"
-
+import { HoverCard, HoverCardContent, HoverCardTrigger } from './components/hover-card'
+import { Command, CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList, CommandSeparator, CommandShortcut } from "./components/command"
+import { Toggle } from "./components/toggle"
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "./components/table"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogOverlay, DialogPortal, DialogTitle, DialogTrigger } from "./components/dialog"
+import { Calendar } from "./components/calendar"
+import { Popover, PopoverContent, PopoverTrigger } from "./components/popover"
+import { Button } from "./components/button"
+import { Input } from "./components/input"
+import { Checkbox } from "./components/checkbox"
+import { Alert, AlertDescription, AlertTitle } from "./components/alert"
+import { Switch } from "./components/switch"
+import { Label } from "./components/label"
+import { Textarea } from "./components/textarea"
 
 export {
-  Avatar, AvatarFallback, AvatarImage,
-
-  Select, SelectContent, SelectItem, SelectTrigger, SelectValue,
-
-  Badge,
-  Progress,
-  Tooltip, TooltipContent, TooltipTrigger,
-  Textarea,
-  Label,
-  Switch,
-  Alert, AlertDescription, AlertTitle,
-  Checkbox,
-  Input,
-  Button,
-  Popover, PopoverTrigger, PopoverContent,
-  Calendar, Dialog, DialogClose, DialogContent, DialogTitle, DialogFooter, DialogHeader, DialogPortal, DialogTrigger, DialogOverlay, DialogDescription,
-  Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField,
-  Table, TableRow, TableBody, TableCell, TableHead, TableFooter, TableHeader, TableCaption,
-  Toggle,
-  Command, CommandItem, CommandList, CommandEmpty, CommandGroup, CommandInput, CommandDialog, CommandShortcut, CommandSeparator,
-
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuCheckboxItem,
-  DropdownMenuRadioItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuGroup,
-  DropdownMenuPortal,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuRadioGroup,
-
-  AlertDialog,
-  AlertDialogPortal,
-  AlertDialogOverlay,
-  AlertDialogTrigger,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogFooter,
-  AlertDialogTitle,
-  AlertDialogDescription,
-  AlertDialogAction,
-  AlertDialogCancel,
+  Avatar, AvatarFallback, AvatarImage, Select, SelectContent, SelectItem, SelectTrigger, SelectValue, Badge, Progress, Tooltip, TooltipContent, TooltipTrigger, Textarea, Label, Switch, Alert, AlertDescription, AlertTitle, Checkbox, Input, Button, Popover, PopoverTrigger, PopoverContent, Calendar, Dialog, DialogClose, DialogContent, DialogTitle, DialogFooter, DialogHeader, DialogPortal, DialogTrigger, DialogOverlay, DialogDescription, Form, FormItem, FormField, FormLabel, FormControl, FormMessage, FormDescription, useFormField, Table, TableRow, TableBody, TableCell, TableHead, TableFooter, TableHeader, TableCaption, Toggle, Command, CommandItem, CommandList, CommandEmpty, CommandGroup, CommandInput, CommandDialog, CommandShortcut, CommandSeparator, DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuCheckboxItem, DropdownMenuRadioItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuGroup, DropdownMenuPortal, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuRadioGroup, AlertDialog, AlertDialogPortal, AlertDialogOverlay, AlertDialogTrigger, AlertDialogContent, AlertDialogHeader, AlertDialogFooter, AlertDialogTitle, AlertDialogDescription, AlertDialogAction, AlertDialogCancel, HoverCard, HoverCardTrigger, HoverCardContent
 }
